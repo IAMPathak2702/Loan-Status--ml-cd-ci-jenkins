1a22a0bfc9f188463dbf059f61d32d715d7b44ca
Ved Prakash Pathak <112847437+IAMPathak2702@users.noreply.github.com>
2024-04-09 01:11:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
-import numpy as np
-import pandas as pd 
 from fastapi.middleware.cors import CORSMiddleware
-from prediction_model.predict import generate_predictions 
+from prediction_model.predict import generate_predictions
 
 app = FastAPI(
     title="Loan Prediction App using API - CI CD Jenkins",
-    description = "A Simple CI CD Demo",
+    description="A Simple CI CD Demo",
     version='1.0'
 )
 
-origins=[
-    "*"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,48 +33,29 @@ class LoanPrediction(BaseModel):
     Credit_History: float
     Property_Area: str
 
-
 @app.get("/")
 def index():
-    return {"message":"Welcome to Loan Prediction App using API - CI CD Jenkins" }
+    return {"message": "Welcome to Loan Prediction App using API - CI CD Jenkins"}
 
 @app.post("/prediction_api")
 def predict(loan_details: LoanPrediction):
-    data = loan_details.model_dump()
+    data = loan_details.dict()
     prediction = generate_predictions([data])["prediction"][0]
     if prediction == "Y":
         pred = "Approved"
     else:
         pred = "Rejected"
-    return {"status":pred}
+    return {"status": pred}
 
 @app.post("/prediction_ui")
-def predict_gui(Gender: str,
-    Married: str,
-    Dependents: str,
-    Education: str,
-    Self_Employed: str,
-    ApplicantIncome: float,
-    CoapplicantIncome: float,
-    LoanAmount: float,
-    Loan_Amount_Term: float,
-    Credit_History: float,
-    Property_Area: str):
-
-    input_data = [Gender, Married,Dependents, Education, Self_Employed,ApplicantIncome,
-     CoapplicantIncome,LoanAmount, Loan_Amount_Term,Credit_History, Property_Area  ]
-    
-    cols = ['Gender', 'Married', 'Dependents', 'Education',
-       'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
-       'Loan_Amount_Term', 'Credit_History', 'Property_Area']
-    
-    data_dict = dict(zip(cols,input_data))
-    prediction = generate_predictions([data_dict])["prediction"][0]
+def predict_gui(loan_details: LoanPrediction):
+    data = loan_details.dict()
+    prediction = generate_predictions([data])["prediction"][0]
     if prediction == "Y":
         pred = "Approved"
     else:
         pred = "Rejected"
-    return {"status":pred}
+    return {"status": pred}
 
-if __name__== "__main__":
-    uvicorn.run(app ,port=9000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
